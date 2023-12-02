b392a95d4500a768510219b5c65316ffa8086c02
Toukara <benjamincms62@gmail.com>
2023-12-02 21:04:36 +0100

    Update : Fetching datas, Table to render players

--- index.js
+++ index.js
@@ -3,60 +3,77 @@ import cheerio from "cheerio";
 import {JSDOM} from "jsdom";
 import * as fs from "fs";
 
-var url = "https://ufolep62tt.org/points-joueurs-2023-2024";
+var url = "https://ufolep62tt.org/points-joueurs-1-1-1";
 
-const response = await axios.get(url);
-
+const response = await axios.get(url, {
+  cookies: {
+    PHPSESSID: "8skvedrj6agf16nfdpngcmf57b",
+    "rgpdKAL-4065-ufolep62tt": "!multiplegtag=true!matomo=true",
+  },
+  headers: {
+    Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Cache-Control": "max-age=0",
+    Connection: "keep-alive",
+    Cookie: "PHPSESSID=8skvedrj6agf16nfdpngcmf57b; rgpdKAL-4065-ufolep62tt=!multiplegtag=true!matomo=true",
+    Referer: "https://www.google.com/",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "cross-site",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 OPR/104.0.0.0",
+    "sec-ch-ua": '"Chromium";v="118", "Opera GX";v="104", "Not=A?Brand";v="99"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+  },
+});
 const dom = new JSDOM(response.data);
 const document = dom.window.document;
 
+const table = document.querySelectorAll("table")[1];
+const rows = Array.from(table.querySelectorAll("tr:not([class])"));
+rows.shift();
+
 const players = [];
 
-async function getPlayersData() {
-  // get the table and convert it to an array
-  const table = document.querySelector("table");
-  const rows = Array.from(table.querySelectorAll("tr"));
+rows.map((row, i) => {
+  if (i === 0 || i === 1) return;
+  const cells = Array.from(row.querySelectorAll("td:not(.table_tdborderv)"));
 
-  // rows.shift();
+  const cellsTrimmed = cells.map((cell) => cell.textContent.trim());
 
-  rows.map((row, i) => {
-    if (i === 0 || i === 1) return;
+  const player = {
+    CLUB: cellsTrimmed[0],
+    FULL_NAME: cellsTrimmed[1],
+    LICENSE_NUMBER: cellsTrimmed[2],
+    POINTS_FORWARD: cellsTrimmed[3],
+    POINTS_BACKWARD: cellsTrimmed[4],
+    FFTT: {
+      FORWARD_POINTS: cellsTrimmed[5],
+      BACKWARD_POINTS: cellsTrimmed[6],
+      CLUB: cellsTrimmed[7],
+    },
+    MUTATION: cellsTrimmed[8],
+  };
 
-    const cells = Array.from(row.querySelectorAll("td"));
-    const cellsTrimed = cells.map((cell) => cell.textContent.trim());
+  players.push(player);
+});
 
-    const player = {
-      club: cellsTrimed[0],
-      name: cellsTrimed[1],
-      ptsFirst: cellsTrimed[2],
-      ptsSecond: cellsTrimed[3],
-      ptsFFTTFirst: cellsTrimed[4],
-      ptsFFTTSecond: cellsTrimed[5],
-      clubFFTT: cellsTrimed[6],
-      wasMoved: cellsTrimed[7],
-    };
-
-    players.push(player);
-    return player;
-  });
-}
-
-await getPlayersData();
-
-try {
+function writeCSV(data) {
   const file = fs.createWriteStream("players.csv");
   file.on("error", (err) => {
     console.log(err);
   });
-  file.write("Nom/Prénom, Classement Debut, Classement Fin, Classement FFTT Debut, Classement FFTT Fin, Club, Club FFTT, Muté\n");
-  players.forEach((player) => {
+  file.write("Club , Nom et Prénom, Classement Aller, Classement Retour, Classement FFTT ph1, Classement FFTT ph2, Club FFTT, Mutation\n");
+  data.forEach((player) => {
     file.write(
-      `${player.name}, ${player.ptsFirst}, ${player.ptsSecond}, ${player.ptsFFTTFirst}, ${player.ptsFFTTSecond}, ${player.club}, ${player.clubFFTT}, ${player.wasMoved}\n`
+      `${player.CLUB}, ${player.FULL_NAME}, ${player.POINTS_FORWARD}, ${player.POINTS_BACKWARD}, ${player.FFTT.FORWARD_POINTS}, ${player.FFTT.BACKWARD_POINTS}, ${player.FFTT.CLUB}, ${player.MUTATION}\n`
     );
   });
   file.end();
 
   console.log("File created");
-} catch (err) {
-  console.log(err);
 }
+
+writeCSV(players);
